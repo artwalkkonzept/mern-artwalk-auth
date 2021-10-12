bb8a83eed5611624a2d4ecc8ab8bab42733f520e
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-10-12 14:04:17 +0200

    First deployment auth

---
+++ server/src/routes/index.js
@@ -0,0 +1,11 @@
+import { logInRoute } from './logInRoute';
+import { signUpRoute } from './signUpRoute';
+import { testRoute } from './testRoute';
+import { updateUserArtwalksRoute } from './updateUserArtwalksRoute';
+
+export const routes = [
+    logInRoute,
+    signUpRoute,
+    testRoute,
+    updateUserArtwalksRoute,
+];

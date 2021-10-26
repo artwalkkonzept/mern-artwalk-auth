4e0fb449527b392b0e89b5fbcf09edc07a9e3138
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-10-26 13:09:44 +0200

    First deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const checkJwt = require("express-jwt"); // Validates access tokens automatically
 
 // Configuration
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json()); // Parse JSON from the request body
@@ -58,5 +58,11 @@ const artwalkRouter = require("./routers/artwalkRouter")(data);
 app.use("/api/users", usersRouter);
 app.use("/api/artwalks", artwalkRouter);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 // Start
 app.listen(port, () => console.log(`Auth Artwalks API running on port ${port}!`));
